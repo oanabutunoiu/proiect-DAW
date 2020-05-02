16648fcaac25de0b2dc4629c0bee825c6a1398ed
oanabutunoiu <61548758+oanabutunoiu@users.noreply.github.com>
2020-05-03 00:09:35 +0300

    fin

--- src/main/resources/static/util.js
+++ src/main/resources/static/util.js
@@ -1,6 +1,8 @@
+var selected;
+
 function studentSelected(){
 		document.getElementById('myButtons').innerHTML = "<input type='button' id='insert' class='ok' value='Insert student' />" + 
 		" <br /> <input type='button' id='update' class='ok' value='Update student information' /> " +
 		" <br /> <input type='button' id='delete' class='ok' value='Delete student' /> ";
-		return $('input[name=student]:checked').val();
+		selected = $('input[name=student]:checked').val();
 }
