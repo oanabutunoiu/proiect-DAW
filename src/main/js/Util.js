ac2f82dc193613238bc6ec71fd4bddbb0c8940d4
oanabutunoiu <61548758+oanabutunoiu@users.noreply.github.com>
2020-05-03 13:11:52 +0300

    dsd

--- src/main/js/Util.js
+++ src/main/js/Util.js
@@ -9,8 +9,8 @@ class Util{
 	
 	studentSelected(){
 		document.getElementById('myButtons').innerHTML = "<input type='button' id='insert' class='ok' value='Insert student'  onClick={util.insertButtonPressed} />" + 
-		"       <input type='button' id='update' class='ok' value='Update student information'  onClick={updateButtonPressed} /> " +
-		"       <input type='button' id='delete' class='ok' value='Delete student' onClick={deleteButtonPressed} /> <br /><br />";
+		"       <input type='button' id='update' class='ok' value='Update student information'  onClick={this.updateButtonPressed} /> " +
+		"       <input type='button' id='delete' class='ok' value='Delete student' onClick={this.deleteButtonPressed} /> <br /><br />";
 		selected = $('input[name=student]:checked').val();
 	}
 	
