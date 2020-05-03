fbaeb858e91e233faa0dbd3edfeec9c9ca35d4d3
oanabutunoiu <61548758+oanabutunoiu@users.noreply.github.com>
2020-05-03 20:56:08 +0300

    Update SecurityConfig.java

--- src/main/java/com/example/proiectDAW/SecurityConfig.java
+++ src/main/java/com/example/proiectDAW/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.proiectDAW;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -19,5 +20,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable();
 	}
 }
