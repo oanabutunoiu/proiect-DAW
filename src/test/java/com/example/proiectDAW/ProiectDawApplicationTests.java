db8168b107e5e218b072af3d441ed45d5888c4f9
oanabutunoiu <61548758+oanabutunoiu@users.noreply.github.com>
2020-05-07 14:12:33 +0300

    Update ProiectDawApplicationTests.java

--- src/test/java/com/example/proiectDAW/ProiectDawApplicationTests.java
+++ src/test/java/com/example/proiectDAW/ProiectDawApplicationTests.java
@@ -1,13 +1,24 @@
 package com.example.proiectDAW;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
+import com.example.proiectDAW.web.*;
 
 @SpringBootTest
 class ProiectDawApplicationTests {
 
+	@Autowired
+	private StudentController studentController;
+	
+	@Autowired
+	private FacultyController facultyController;
+	
 	@Test
 	void contextLoads() {
+		assertThat(studentController).isNotNull();
+		assertThat(facultyController).isNotNull();
 	}
 
 }
